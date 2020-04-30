6dc354017bf21eb8d2de026c2f9707cb6b0266a9
manhtai <vo@manhtai.com>
2020-04-30 20:26:00 +0700

    Auth user

--- lib/yumbl_web/controllers/user_controller.ex
+++ lib/yumbl_web/controllers/user_controller.ex
@@ -4,6 +4,8 @@ defmodule YumblWeb.UserController do
   alias Yumbl.Accounts
   alias Yumbl.Accounts.User
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
@@ -23,11 +25,23 @@ defmodule YumblWeb.UserController do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> YumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} is created!")
         |> redirect(to: Routes.user_path(conn, :index))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "Please login first")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
   end
 end
