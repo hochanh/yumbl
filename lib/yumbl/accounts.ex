4e61d3098f57d21429837a0fa8515f2ce547d313
manhtai <vo@manhtai.com>
2020-04-30 09:10:11 +0700

    Get users from db

--- lib/yumbl/accounts.ex
+++ lib/yumbl/accounts.ex
@@ -1,22 +1,16 @@
 defmodule Yumbl.Accounts do
-
+  alias Yumbl.Repo
   alias Yumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "David", username: "david"},
-      %User{id: "2", name: "Tommy", username: "tom"},
-      %User{id: "3", name: "Joe", username: "joe"},
-    ]
-  end
-
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users() do
+    Repo.all(User)
   end
 end
